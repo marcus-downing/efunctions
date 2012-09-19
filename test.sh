4076160e8a6fd9c2aead50660bdfa74613815819
Marcus Downing <mdowning@potassium.(none)>
2012-09-19 12:48:00 +0100

    Degrade nicely in a non-interactive or non-coloured terminal

--- test.sh
+++ test.sh
@@ -1,15 +1,25 @@
-source functions.sh
-eindent
+source /etc/init.d/functions.sh
 einfo "Foo"
 eindent
 ewarn "Bar"
 eoutdent
 eerror "Qux"
+
+echo ""
 ebegin "Ged"
 eend 0
+eindent
 ebegin "Mog"
 eend 1
+ebegin "Nib"
+eindent
+eend 1 "Fail!"
+eoutdent
+ebegin "Kaa"
+eindent
+ewend 1 "Provisos"
+eoutdent
+eoutdent
 
 echo ""
 eval_ecolors
-eoutdent
