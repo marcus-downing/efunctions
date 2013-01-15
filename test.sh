8b36ee496c5d22f6346e628ccd911a39e4aae298
Daniel Llewellyn <diddledan@gmail.com>
2013-01-15 21:09:38 +0000

    fixed issue #3

--- test.sh
+++ test.sh
@@ -1,15 +1,30 @@
-source functions.sh
-eindent
+source /etc/init.d/functions.sh
 einfo "Foo"
+einfo Spaces rock
 eindent
 ewarn "Bar"
+ewarn Spaces rock
 eoutdent
 eerror "Qux"
+eerror Spaces rock
+
+echo ""
 ebegin "Ged"
 eend 0
+eindent
 ebegin "Mog"
 eend 1
+ebegin "Nib"
+eindent
+eend 1 "Fail!"
+eoutdent
+ebegin "Kaa"
+eindent
+ewend 1 "Provisos"
+eoutdent
+eoutdent
+ebegin Spaces rock
+eend 1 Yes indeedy!
 
 echo ""
 eval_ecolors
-eoutdent
