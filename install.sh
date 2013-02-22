fd5f528b0995d1ef7984cf6f125ff8e48f3c898f
Marcus Downing <marcus@bang-on.net>
2013-02-22 14:17:29 +0000

    Install script: check file

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 HERE=$(readlink -f "$0")
 DIR=$(dirname "$HERE")
 SCRIPT="$DIR/functions.sh"
-echo "Installing $SCRIPT into /etc/init.d"
+
+if [ -f /etc/init.d/functions.sh ]; then
+  "$DIR/efunctions/eerror" "Cannot install efunctions: file already exists"
+fi
+
+"$DIR/efunctions/einfo" "Installing $SCRIPT into /etc/init.d"
 sudo ln -s "$SCRIPT" /etc/init.d/functions.sh
