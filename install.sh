3e6b2819e8cbd4e28b8ee52120a81bdd0f488a89
Marcus Downing <marcus@bang-on.net>
2013-02-22 14:18:20 +0000

    Install script: Fix output

--- install.sh
+++ install.sh
@@ -2,8 +2,11 @@ HERE=$(readlink -f "$0")
 DIR=$(dirname "$HERE")
 SCRIPT="$DIR/functions.sh"
 
+source "$SCRIPT"
+
 if [ -f /etc/init.d/functions.sh ]; then
   "$DIR/efunctions/eerror" "Cannot install efunctions: file already exists"
+  exit
 fi
 
 "$DIR/efunctions/einfo" "Installing $SCRIPT into /etc/init.d"
